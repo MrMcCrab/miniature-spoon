2230787f19602925d7f653ecc3aa8a3a0d78b0e2
MrMcCrab <>
2017-04-01 18:36:10 +0300

    Add threds to database

--- project/postThread.php
+++ project/postThread.php
@@ -1,9 +1,15 @@
 <?php
   include 'session.php';
+  include 'connect.php';
 
   $threadTitle = htmlspecialchars($_POST['topic']);
   $threadContent = htmlspecialchars($_POST['message']);
+  $threadStarter = $_SESSION['login_user'];
 
+  $stmt_thread = $db->prepare("insert into threads (thread_title, thread_starter, thread_content) values (?, ?, ?)");
+  $stmt_thread->bind_param("sss", $threadTitle, $threadStarter, $threadContent);
+  $stmt_thread->execute();
 
-  
+  header ("refresh:2, url=index.php");
+
 ?>
