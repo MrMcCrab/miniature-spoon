224a449a777d4eae6473be9a1308903a09b9d484
MrMcCrab <>
2017-05-03 15:56:29 +0300

    php script to get posts by thread

---
+++ project/getPosts.php
@@ -0,0 +1,36 @@
+<?php
+
+$id = $_GET['id'];
+
+$sql = "select * from threads where thread_id = '$id'";
+$result = $db->query($sql);
+
+$sql2 = "select * from messages where thread_id = '$id'";
+$result2 = $db->query($sql2);
+
+if($result->num_rows > 0){
+  while ($row = $result->fetch_assoc()) {
+    echo "
+      <div class='row threadContainer'>
+        <div class=col-md-12>
+          $row[thread_title]
+        </div>
+        <div class='col-md-12'>
+          $row[thread_content]
+        </div>
+      </div>";
+  }
+}
+if($result2->num_rows > 0){
+  while ($row2 = $result2->fetch_assoc()) {
+    echo "
+      <div class='col-md-2'>
+        $row2[user_id]
+      </div>
+      <div class='col-md-10'>
+        $row2[message_content]
+      </div>";
+  }
+}
+
+?>
