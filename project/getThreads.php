69c6ad82b37b63ee94b92081597db8f3108d806a
MrMcCrab <>
2017-05-03 15:58:14 +0300

    Updated html

--- project/getThreads.php
+++ project/getThreads.php
@@ -12,28 +12,22 @@
     while($row = $result->fetch_assoc()){
       //echo $row["thread_title"] . $row["thread_starter"] . $row["thread_content"];
       echo"
-      <div class='row background'>
 
-        <div class='col-md-12 thread'>
-          <div class='row'>
+        <div class='row threadContainer'>
+          <div class=col-md-12>
+            <h4><a  href='viewThread.php?id=$row[thread_id]&title=$row[thread_title]'>$row[thread_title]</a></h4>
+          </div>
 
-            <div class='col-md-2 title'>
-              $row[thread_title]
-            </div>
-
-            <div class='col-md-8'>
-
-            </div>
-
-            <div class='col-md-2 starter'>
-              $row[thread_starter]
-            </div>
-
+          <div class='col-md-4'>
+            <p class='threadStarter'>$row[thread_starter]</p>
+          </div>
+          <div class='col-md-4'>
 
           </div>
-        </div>
-
-      </div>";
+          <div class=col-md-4>
+            <p class='threadId'>#$row[thread_id]</p>
+          </div>
+        </div>";
     }
   }
  ?>
