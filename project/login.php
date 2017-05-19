03aa1c1e0ace021fc796d1af9413c412ffd735ab
MrMcCrab <>
2017-05-19 14:38:32 +0300

    Updated login to use hashed password

--- project/login.php
+++ project/login.php
@@ -18,14 +18,19 @@
       $l_password = mysqli_real_escape_string($db, $_POST['password']);
 
       //Check if login credentails match database
-      $sql = "SELECT user_name FROM users WHERE user_name = '$l_username' and user_pass = '$l_password'";
+      //$sql = "SELECT user_name FROM users WHERE user_name = '$l_username' and user_pass = '$l_password'";
+      $sql = "SELECT user_pass FROM users WHERE user_name = '$l_username'";
       $result = mysqli_query($db, $sql);
       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+      $hashFromDb = $row['user_pass'];
 
-      $count = mysqli_num_rows($result);
+      $passwordHash = password_verify($l_password, $hashFromDb);
+
+      //$count = mysqli_num_rows($result);
       // Returns only one result if username and password matches
 
-      if($count == 1) {
+      //if($count == 1) {
+        if($passwordHash){
 
         $_SESSION['login_user'] = $l_username;
 
